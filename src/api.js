947c39495469d81e21375bc3ee46fa9f3496fa24
Geeta <vgeetachahar@gmail.com>
2022-12-12 23:03:26 +0530

    Save

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '8cc97f1725msh13b0785e2c8f01fp1bc6ebjsn0e8663d581f5',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
